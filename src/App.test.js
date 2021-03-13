b1f5a1f879414ef34acc3e062608405a64a30025
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2021-03-13 08:39:10 +0100

    Update test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,8 @@ import App from "./App";
 
 test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/Learn DevOps and .../i);
+  const linkElement = screen.getByText(
+    /Learn DevOps and ... test jenkins deploy/i
+  );
   expect(linkElement).toBeInTheDocument();
 });
